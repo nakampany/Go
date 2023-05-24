2efb8eea6c080a99dbca272857ef790f7edbc5b6
nakampany <nakajima.shota@hrbrain.co.jp>
2023-05-24 14:40:36 +0900

    hogehoge

--- Golong/section04/main.go
+++ Golong/section04/main.go
@@ -5,7 +5,11 @@ import (
 	"strconv"
 )
 
+/*
+型
+*/
 func main() {
+	//
 	var i int = 123
 	var toString_s string
 	var toString_Unicode string
@@ -15,4 +19,19 @@ func main() {
 	fmt.Println(i)                // int 123
 	fmt.Println(toString_s)       // string "123"
 	fmt.Println(toString_Unicode) // 123がrune(Unicode)として認識され "{" になるので注意!!
+
+	// 文字列 (String) → 数値 (int)
+	var s string = "123"
+	var s2 string = "Draemon"
+	var Toint_s int
+	var Toint_err int
+
+	Toint_s, _ = strconv.Atoi(s)
+	fmt.Println(Toint_s) // int 123
+
+	var err error
+
+	Toint_err, _ = strconv.Atoi(s2)
+	fmt.Println(Toint_err)
+	fmt.Println(err)
 }
