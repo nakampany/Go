0bd7a5d8eb1041abededc769df03c4a1434d0e35
nakampany <nakajima.shota@hrbrain.co.jp>
2023-05-31 13:08:31 +0900

    hogehoge

--- Golong/section12/main.go
+++ Golong/section12/main.go
@@ -17,18 +17,25 @@ type Car struct {
 }
 
 func (p *Person) ToString() string {
-	return fmt.Sprintf(p.Name, p.Age)
+	return fmt.Sprintf("%s %d", p.Name, p.Age)
 }
 func (p *Car) ToString() string {
-	return fmt.Sprintf(p.Number, p.Model)
+	return fmt.Sprintf("%s %s", p.Number, p.Model)
 }
+
+// func (p *Person) ToString() string {
+// 	return fmt.Sprintf(p.Name, p.Age)
+// }
+// func (p *Car) ToString() string {
+// 	return fmt.Sprintf(p.Number, p.Model)
+// }
+//  a%!(EXTRA int=1)
+//  1%!(EXTRA string=1)
 
 func main() {
 	v := []Strings{&Person{Name: "a", Age: 1}, &Car{Number: "1", Model: "1"}}
 
 	for _, s := range v {
-		fmt.Println(s)
+		fmt.Println(s.ToString())
 	}
-	// &{a 1}
-	// &{1 1}
 }
