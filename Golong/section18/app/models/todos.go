8b30d3d69e284211411f5b041da7eb5829b94088
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-01 14:34:17 +0900

    GetTodo

--- Golong/section18/app/models/todos.go
+++ Golong/section18/app/models/todos.go
@@ -24,3 +24,17 @@ func (u *User) CreateTodo(content string) (err error) {
 	}
 	return err
 }
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select * from todos
+	where id = ?`
+	todo = Todo{}
+
+	err = Db.QueryRow(cmd, id).Scan(
+		&todo.ID,
+		&todo.Content,
+		&todo.UserID,
+		&todo.CreatedAt)
+
+	return todo, err
+}
