3616853cfb7c138034ed84ab0ec6a06251d59e1a
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-01 10:39:33 +0900

    hogehoge

---
+++ Golong/section18/go.mod
@@ -0,0 +1,7 @@
+module udemy/Golong/section18
+
+go 1.20
+
+require gopkg.in/go-ini/ini.v1 v1.67.0
+
+require github.com/stretchr/testify v1.8.4 // indirect
--- Golong/section18/main.go
+++ Golong/section18/main.go
@@ -1,1 +1,14 @@
+package main
 
+import (
+	"fmt"
+
+	"udemy/Golong/section18/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
