96378fdd9ad0ddf759187da305b79d3fe35799ca
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-05 14:30:24 +0900

    hoge

--- Golong/section18/app/controllers/route_auth.go
+++ Golong/section18/app/controllers/route_auth.go
@@ -8,6 +8,6 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, "Hello", "layout", "public_navbar", "signup")
 }
 
-func login(w *http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "public_navbar", "login")
 }
