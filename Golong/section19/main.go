39406812bcd0ff4d32126aaeb78e38faa2459b0f
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-07 15:42:35 +0900

    hoge

--- Golong/section19/main.go
+++ Golong/section19/main.go
@@ -19,6 +19,8 @@ func Add[T Number](a, b T) T {
 	return a + b
 }
 
+// 3
+// Underlying Typeのみ許可
 type Number2 interface {
 	~int | ~int32 | ~int64 | ~float32 | ~float64
 }
@@ -29,7 +31,7 @@ func Add2[T Number2](a, b T) T {
 
 type MyInt int
 
-// 3
+// 4
 type SampleType interface {
 	string | int
 }
@@ -39,13 +41,14 @@ func sampleFuncGenerics[T SampleType](x T) T {
 }
 
 func main() {
+	// 1
 	PrintSlice[int]([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	PrintSlice[string]([]string{"a", "b", "c", "d", "e", "f", "g", "h", "i"})
-
+	// 2
 	fmt.Println(Add[int](1, 2))
-
+	// 3
 	fmt.Println(Add2[MyInt](1, 2))
-
+	// 4
 	fmt.Println(sampleFuncGenerics[int](1))
 	fmt.Println(sampleFuncGenerics[string]("a"))
 
