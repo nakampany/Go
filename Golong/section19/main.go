72738dc66c4243e333c08601cb9e2119774445b1
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-07 15:34:51 +0900

    hoge

--- Golong/section19/main.go
+++ Golong/section19/main.go
@@ -9,7 +9,17 @@ func PrintSlice[T any](s []T) {
 	}
 }
 
+type Number interface {
+	int | int32 | int64 | float32 | float64
+}
+
+func Add[T Number](a, b T) T {
+	return a + b
+}
+
 func main() {
 	PrintSlice[int]([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	PrintSlice[string]([]string{"a", "b", "c", "d", "e", "f", "g", "h", "i"})
+
+	fmt.Println(Add[int](1, 2))
 }
