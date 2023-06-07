cf12e509d91f852c708623d1cedb6e4798cb468f
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-07 15:34:39 +0900

    hoge

--- Golong/section19/main.go
+++ Golong/section19/main.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+// 引数の型を[T any](s []T)にすることで、どんな型でも受け取れるようになる
+func PrintSlice[T any](s []T) {
+	for _, v := range s {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	PrintSlice[int]([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	PrintSlice[string]([]string{"a", "b", "c", "d", "e", "f", "g", "h", "i"})
+}
