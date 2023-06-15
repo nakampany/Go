0e0611324ad11d1dcf400c6f73f752359d1d3350
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-15 15:28:35 +0900

    hoge

--- Golong/TodoApp/go/main.go
+++ Golong/TodoApp/go/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"udemy/Golong/TodoApp/go/app/controllers"
 	"udemy/Golong/TodoApp/go/app/models"
 	"udemy/Golong/TodoApp/go/utils"
 )
@@ -13,18 +13,6 @@ func init() {
 
 // メイン関数
 func main() {
-
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	StartMainServer()
-}
-
-func StartMainServer() error {
-	http.HandleFunc("/", handler)
-	return http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! Path is %s", r.URL.Path[:])
+	fmt.Println(models.Db)
+	controllers.StartMainServer()
 }
