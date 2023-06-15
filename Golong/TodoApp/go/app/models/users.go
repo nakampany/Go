0851d6e70121cbb2779aa910a1f15154d7519f6d
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-15 15:28:49 +0900

    hoge

--- Golong/TodoApp/go/app/models/users.go
+++ Golong/TodoApp/go/app/models/users.go
@@ -14,6 +14,14 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	ID        int
+	UUID      string
+	Email     string
+	UserID    int
+	CreatedAt time.Time
+}
+
 func (u *User) CreateUser() (err error) {
 	cmd := `insert into users (uuid, name, email, password created_at) values (?, ?, ?, ?, ?)`
 
@@ -48,4 +56,94 @@ func (u *User) DeleteUser() (err error) {
 		log.Fatalln(err)
 	}
 	return err
+}
+
+// select -> queryRow -> scan
+func GetUserByEmail(email string) (user User, err error) {
+	user = User{}
+	cmd := `select * from users where email = ?`
+	err = Db.QueryRow(cmd, email).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	return user, err
+}
+
+// session作成
+func (u *User) CreateSession() (session Session, err error) {
+	cmd1 := `insert into sessions (
+		uuid,
+		email,
+		user_id,
+		created_at
+		) values (?, ?, ?, ?)`
+
+	_, err = Db.Exec(cmd1,
+		createUUID(),
+		u.Email,
+		u.ID,
+		time.Now())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmd2 := `select * from sessions where user_id = ? and email = ?`
+	err = Db.QueryRow(cmd2, u.ID, u.Email).Scan(
+		&session.ID,
+		&session.UUID,
+		&session.Email,
+		&session.UserID,
+		&session.CreatedAt,
+	)
+	return session, err
+}
+
+// sessionを取得
+func (sess *Session) CheckSession() (valid bool, err error) {
+	cmd := `select * from sessions where uuid = ?`
+	err = Db.QueryRow(cmd, sess.UUID).Scan(
+		&sess.ID,
+		&sess.UUID,
+		&sess.Email,
+		&sess.UserID,
+		&sess.CreatedAt,
+	)
+	if err != nil {
+		valid = false
+		return valid, err
+	}
+	if sess.ID != 0 {
+		valid = true
+	}
+	return valid, err
+}
+
+// sessionを削除
+func (sess *Session) DeleteSessionByUUID() (err error) {
+	cmd := `delete from sessions where uuid = ?`
+	_, err = Db.Exec(cmd, sess.UUID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+// sessionからユーザーを取得
+func (sess *Session) GetUserBySession() (user User, err error) {
+	user = User{}
+	cmd := `select * FROM users
+	where id = ?`
+	err = Db.QueryRow(cmd, sess.UserID).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.CreatedAt)
+
+	return user, err
 }
